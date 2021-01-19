691bcbd0a667af6eba26745ff291285d0d490a17
imbacc <873243031@qq.com>
2021-01-19 21:34:55 +0800

    update 01/19

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,26 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '../components/HelloWorld.vue'; // @ is an alias to /src
+
+import { defineComponent, onMounted, getCurrentInstance } from 'vue'
+import HelloWorld from '../components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
   },
+  setup(props, context) {
+			const { ctx } = getCurrentInstance()
+
+			onMounted(()=>{
+				ctx.is_action('app_111', {}, {})
+				ctx.is_action('app_222', { _id: 1111 }, {})
+				ctx.is_action('app_333', { _id: 1111 }, {}, 'GET')
+				ctx.is_action('app_444', {}, {}, 'GET')
+				ctx.is_action('app_555', {}, {})
+				ctx.is_action('app_666', { _id: 1111 }, {})
+			})
+		}
 });
 </script>
